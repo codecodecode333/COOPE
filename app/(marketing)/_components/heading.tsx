1a5f207b294a785df9ea098f3a3a33572851703f
codecodecode333 <gksrnr2305@gmail.com>
2025-01-23 18:10:46 +0900

    tem

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,19 +1,41 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     return (
         <div className="max-w-3xl space-y-4">
             <h3 className="text-base sm:text-xl md:text-2xl
             font-medium">
                 협업을 새롭게 정의하다
             </h3>
-            <Button>
-                시작하기
-                <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
+            { isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg"/>
+                </div>
+            )}
+            { isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        시작하기
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton>
+                    <Button>
+                        Get Coope free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
