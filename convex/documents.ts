73c18d54008db2fa64f41630e0bf369b2e3eaea6
codecodecode333 <gksrnr2305@gmail.com>
2025-02-05 17:44:57 +0900

    Schema, API

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,21 @@
-/*import { v } from "convex/values";
+import { v } from "convex/values";
 
 import { mutation, query} from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
+
+export const get = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+        
+        const documents = await ctx.db.query("documents").collect();
+
+        return documents;
+    }
+})
 
 export const create = mutation({
     args: {
@@ -27,4 +41,4 @@ export const create = mutation({
 
         return document;
     }
-})*/
+})
